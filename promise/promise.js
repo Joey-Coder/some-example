335e38cff49c058c382c5b00a354606cee0e90a1
Joey-Coder <joey950326@gmail.com>
2021-04-15 17:36:48 +0800

    完成Promise then功能

---
+++ promise/promise.js
@@ -0,0 +1,151 @@
+(function (window) {
+  /**
+   * Promise 构造函数
+   * @param {function} excutor 内部执行的构造函数
+   */
+  function Promise(excutor) {
+    const self = this;
+    self.PromiseState = "pending"; // 状态值，有fulfilled和rejected
+    self.PromiseResult = undefined;
+    self.callbacks = [];
+
+    /**
+     * 定义Promise中的resolve方法，在excutor中调用
+     * @param {any} value
+     */
+    function resolve(value) {
+      if (self.PromiseState !== "pending") {
+        // 如果当前不是pending, 直接结束
+        return;
+      }
+      self.PromiseState = "fulfilled";
+      self.PromiseResult = value;
+
+      // 异步调用所有待处理的onRejcted回调函数
+      if (self.callbacks.length > 0) {
+        setTimeout(() => {
+          self.callbacks.forEach((obj) => {
+            obj.onResolved();
+          });
+        });
+      }
+    }
+
+    /**
+     * 定义Promise中的reject方法，在excutor中调用
+     * @param {any} reason
+     */
+    function reject(reason) {
+      if (self.PromiseState !== "pending") {
+        return;
+      }
+      self.PromiseState = "rejected";
+      self.PromiseResult = reason;
+
+      // 异步调用所有待处理的onRejected
+      if (self.callbacks.length > 0) {
+        setTimeout(() => {
+          self.callbacks.forEach((obj) => {
+            obj.onRejected();
+          });
+        });
+      }
+    }
+
+    try {
+      // 立即同步调用excutor处理
+      excutor(resolve, reject);
+    } catch (error) {
+      // 如果抛出了异常，直接失败
+      reject(error);
+    }
+  }
+
+  /**
+   * 为promise指定成功/失败的回调函数
+   * 函数的返回值是一个新的promise对象
+   * @param {function} onResolved
+   * @param {function} onRejected
+   * @returns {Promise}
+   */
+
+  Promise.prototype.then = function (onResolved, onRejected) {
+    const self = this;
+    // 如果onResolved/onRejected不是函数，为它指定一个默认函数
+    onResolved =
+      typeof onResolved === "function" ? onResolved : (value) => value;
+    onRejected =
+      typeof onRejected === "function"
+        ? onRejected
+        : (reason) => {
+            throw reason;
+          };
+
+    return new Promise((resolve, reject) => {
+      function handle(callback) {
+        try {
+          let result = callback(self.PromiseResult);
+          if (result instanceof Promise) {
+            result.then(resolve, reject);
+          } else {
+            resolve(result);
+          }
+        } catch (err) {
+          reject(err);
+        }
+      }
+      // 处理resolve
+      if (self.PromiseState === "fulfilled") {
+        handle(onResolved);
+      } else if (self.PromiseState === "rejected") {
+        // 处理reject和throw
+        handle(onRejected);
+      } else if (self.PromiseState === "pending") {
+        // 处理异步setTimeout
+        // 收集onResolved/onRejected
+        self.callbacks.push({
+          onResolved: function () {
+            handle(onResolved);
+          },
+          onRejected: function () {
+            handle(onRejected);
+          },
+        });
+      }
+    });
+  };
+
+  /**
+   * 为promise指定失败的回调函数
+   * 是then(null, onRejected)的语法糖
+   * @param {function} onRejected
+   */
+  Promise.prototype.catch = function (onRejected) {};
+
+  /**
+   * 返回一个指定了成功的value的promise对象
+   * @param {any} value
+   */
+  Promise.resolve = function (value) {};
+
+  /**
+   * 返回一个指定了失败reason的promise对象
+   * @param {any} reason
+   */
+  Promise.reject = function (reason) {};
+
+  /**
+   * 执行全部promises
+   * @param {Array} promises
+   */
+  Promise.all = function (promises) {};
+
+  /**
+   * 优先返回第一个执行promises
+   * @param {Array} promises
+   */
+  Promise.race = function (promises) {};
+
+  // 暴露构造函数
+  window.Promise = Promise;
+})(window);
